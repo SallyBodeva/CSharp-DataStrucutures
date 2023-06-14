7c4963a301d8e6a221103aab672dfb89bdbd0976
salihe <salihe.bodeva10@gmail.com>
2023-06-14 23:58:10 +0300

    exercise

---
+++ S08-TreesAndGraphs/P00-DemoGraphs/Graph.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+public class Graph<T>
+{
+    private Dictionary<T, List<T>> adjacencyList;
+    private int count;
+
+    public int Count { get => adjacencyList.Count; }
+    public Graph()
+    {
+        adjacencyList = new Dictionary<T, List<T>>();
+    }
+    public void AddVertex(T vertex)
+    {
+        if (!adjacencyList.ContainsKey(vertex))
+        {
+            adjacencyList[vertex] = new List<T>();
+        }
+    }
+    public void AddEdge(T source, T destination)
+    {
+        if (!adjacencyList.ContainsKey(source))
+        {
+            adjacencyList[source] = new List<T>();
+        }
+        if (!adjacencyList.ContainsKey(destination))
+        {
+            adjacencyList[destination] = new List<T>();
+        }
+        adjacencyList[source].Add(destination);
+        adjacencyList[destination].Add(source);
+    }
+    public void RemoveVertex(T vertex)
+    {
+        if (adjacencyList.ContainsKey(vertex))
+        {
+            List<T> neihgbors = adjacencyList[vertex];
+            foreach (var item in neihgbors)
+            {
+                adjacencyList[item].Remove(vertex);
+            }
+        }
+        adjacencyList.Remove(vertex);
+    }
+    public void RemoveEdge(T source, T destionation)
+    {
+        if (adjacencyList.ContainsKey(source) && adjacencyList.ContainsKey(destionation))
+        {
+            adjacencyList[source].Remove(destionation);
+            adjacencyList[destionation].Remove(source);
+        }
+    }
+    public void Test(T vetrex)
+    {
+        Console.WriteLine(String.Join(" ", adjacencyList[vetrex]));
+    }
+    public List<T> GetNeihghbors(T vertex)
+    {
+        if (adjacencyList.ContainsKey(vertex))
+        {
+            return adjacencyList[vertex];
+        }
+        return new List<T>();
+    }
+    public List<T> GetVertixes()
+    {
+        return adjacencyList.Keys.ToList();
+    }
+    public bool HasEdge(T source, T destination)
+    {
+        if (adjacencyList.ContainsKey(source) && adjacencyList.ContainsKey(destination))
+        {
+            if (adjacencyList[source].Contains(destination) && adjacencyList[destination].Contains(source))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ S08-TreesAndGraphs/P00-DemoGraphs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+public class Program
+{
+    static void Main()
+    {
+        Graph<int> g = new Graph<int>();
+        g.AddVertex(3);
+        g.AddVertex(4);
+        g.AddVertex(5);
+        g.AddVertex(6);
+        g.AddVertex(7);
+
+        g.AddEdge(4, 5);
+        g.AddEdge(5, 6);
+        g.AddEdge(6, 7);
+        //int count1 = g.Count;
+        //Console.WriteLine(count1) ;
+        //g.Test(5);
+        //g.RemoveVertex(4);
+        //int count2 = g.Count;
+        //Console.WriteLine(count2);
+        //g.Test(5);
+        //Console.WriteLine(string.Join(" ",g.GetVertixes()));
+        //Console.WriteLine(string.Join(" ",g.GetNeihghbors(23)));
+        //Console.WriteLine(string.Join(" ", g.GetVertixes()));
+        Console.WriteLine(g.HasEdge(4,5));
+    }
+}
+
