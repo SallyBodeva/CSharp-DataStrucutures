89e4eb7d33d1c0887c4d2b9c4367a941a825b96d
salihe <salihe.bodeva10@gmail.com>
2023-09-04 13:00:48 +0300

    Working with streams...

---
+++ Streams, Files and Directories/P01-EvenLines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01_EvenLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
